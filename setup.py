86030678d14fe4cc3fd2369777da1b89b1bf0255
Bernardo Heynemann <heynemann@gmail.com>
2009-02-17 23:01:34 +0000

    Released to pypi 0.1.

--- setup.py
+++ setup.py
@@ -7,7 +7,32 @@ setup(name='PyoC',
       version=version,
       description="PyoC is an IoC container for Python projects",
       long_description="""\
-To be determined.""",
+============
+Introduction
+============
+PyoC is an IoC container for Python. It's purpose is to make it even easier (it's easy already in Python) to manage dependencies between components. 
+
+It's heavily oriented towards a Convention-over-Configuration approach.
+
+===================
+Project Cheat Sheet
+===================
+
+Project Google Groups Page - http://groups.google.com/group/pythonioc
+
+Project Conventions: http://groups.google.com/group/pythonioc/web/conventions
+
+Links and Blog Posts: http://groups.google.com/group/pythonioc/web/Links%20and%20Blog%20Posts
+
+Project JIRA (Issue and Version Management) - 
+http://jira.stormwindproject.org:8080/browse/PYOC
+
+Project Subversion Server: http://svn.stormwindproject.org/svn/PyoC/Trunk/ (``svn 
+co http://svn.stormwindproject.org/svn/PyoC/Trunk/ PyoC``)
+
+**PyPI Page**: -
+
+**Docs for current version**: -""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='IoC Dependency Injection',
       author='Bernardo Heynemann',
