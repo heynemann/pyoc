ffafd2c8450187d193c8d5bfdfbf7006d3769c16
Bernardo Heynemann <heynemann@gmail.com>
2009-03-06 23:00:09 -0300

    Added test for reflection class

---
+++ tests/test_reflection.py
@@ -0,0 +1,11 @@
+import unittest
+import sys
+import os
+root_path = os.path.abspath(__file__ + "/../../../")
+sys.path.insert(0, root_path)
+from pyoc.ioc import *
+
+class TestReflection(unittest.TestCase):
+    def setUp(self):
+        pass
+    
